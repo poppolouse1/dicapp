7b9d9fdb26278583bce215163edfb126847df6e8
Özgü Arda Türkmen <79911373+poppolouse1@users.noreply.github.com>
2021-05-07 19:13:14 +0300

    Add files via upload

---
+++ Word_types.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DicApp
+{
+    class Word
+    {
+        public string text;
+        //heello
+        public Word(string text)
+        {
+            this.text = text;
+        }
+
+        
+    }
+
+    class Noun : Word
+    {
+        public string article;
+        public string plural;
+        public string englishMeaning;
+        public string[] otherMeanings;
+
+        public Noun(string text, string article, string plural, string englishMeaning, string[] otherMeanings) : base(text)
+        {
+            this.article = article;
+            this.plural = plural;
+            this.englishMeaning = englishMeaning;
+            this.otherMeanings = otherMeanings;
+        }
+    }
+
+    class Verb : Word
+    {
+        public string verbType;
+        public string mainMeaning;
+        public string[] otherMeanings;
+
+        public Verb(string text, string verbType, string mainMeaning, string[] otherMeanings) : base(text)
+        {
+            this.verbType = verbType;
+            this.mainMeaning = mainMeaning;
+            this.otherMeanings = otherMeanings;
+        }
+    }
+
+    class Adjective : Word
+    {
+        public string mainMeaning;
+        public string[] otherMeanings;
+
+        public Adjective(string text, string mainMeaning, string[] otherMeanings) : base(text)
+        {
+            this.mainMeaning = mainMeaning;
+            this.otherMeanings = otherMeanings;
+        }
+    }
+}
---
+++ functions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DicApp
+{
+    class functions
+    {
+        public void writeToFile(string wordProps, string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(@"Dictionary/Deutsch/" + filePath, true))
+            {
+                writer.WriteLine(wordProps);
+            }
+
+        }
+
+        public string loadLanguageText(string language, int lineNumber)
+        {
+
+            using (StreamReader reader = new StreamReader("Language/" + language))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = line.Replace("\\n", "\n");
+                    string[] lineSep = line.Split('#');
+                    if (int.Parse(lineSep[0]) == lineNumber)
+                    {
+                        return lineSep[1];
+                    }
+                }
+            }
+            /*
+            string[] languageLines = File.ReadAllLines("Language/" + language);
+
+            foreach (string line in languageLines)
+            {
+                
+            }
+            */
+            return "ERROR";
+
+        }
+    }
+}
